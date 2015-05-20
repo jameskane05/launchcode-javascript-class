de7f477e1173220065f880f6b37ceaa70d8c5023
jameskane05 <jameskane05@gmail.com>
2015-05-20 16:47:03 -0500

    Updating w/ changes made Wednesday in class

--- getitdone-jquery/js/getitdone.js
+++ getitdone-jquery/js/getitdone.js
@@ -3,32 +3,48 @@ $(document).ready(function() {	// waits for whole page to load
         function() {
             var task = $('#taskEntry').val();  // store the submitted value
 
-            // func to run when tasks are completed as a .click callback
-            var taskDone = function () {
-                $('#done ul').append(this);
-                $('.glyphicon-ok').remove();
-                $('#done ul li').append(refreshIcon);
-                $(this).css("text-decoration", "line-through");
-            };
+            if (task != '') {
+                // func to run when tasks are completed
+                var taskDone = function () {
+                    $('#done ul').append(this.parentNode);
+                    $('.glyphicon-ok', '#done').remove();
+                    $(newTask).append(repeatIcon.click(taskRepeat));
+                };
 
-            // func that puts the task back on the #list
-            var taskRepeat = function () {
-                $('#list ul').append(this);
-                $('.glyphicon-repeat').remove();
-                $('#list ul li').append(doneIcon);
-                $('#list ul li').css("text-decoration", "none");
-            };
+                // func that puts the task back on the #list
+                var taskRepeat = function () {
+                    $('#list ul').append(this.parentNode);
+                    $('.glyphicon-repeat', '#list').remove();
+                    $(newTask).append(doneIcon.click(taskDone));
+                };
 
-            //glyphicon HTML vars and click behaviors
-            var deleteIcon = $('<span class="glyphicon glyphicon-trash"></span>');
-            var doneIcon = $('<span class="glyphicon glyphicon-ok"></span>').click(taskDone());
-            var refreshIcon = $('<span class="glyphicon glyphicon-repeat"></span>');
+                // func that removes
+                var taskDelete = function () {
+                    $(this.parentNode).remove();
+                }
 
-            //new task, icon HTML, plus click behaviors
-            var newTask = $('<li class="task">' + task + '</li>').append(doneIcon).append(deleteIcon);
+                //glyphicon HTML vars and .click callback funcs
+                var deleteIcon = $('<span class="glyphicon glyphicon-trash"></span>').click(taskDelete);
+                var doneIcon = $('<span class="glyphicon glyphicon-ok"></span>').click(taskDone);
+                var repeatIcon = $('<span class="glyphicon glyphicon-repeat"></span>').click(taskRepeat);
+                var currDate = new Date($.now());
 
-            $('#list ul').append(newTask);  // add newTask
+                if (currDate.getHours() > 12) var AMPM = " p.m.";
+                else var AMPM = " a.m.";
+
+                if (currDate.getMinutes() < 10) var minutes = "0" + currDate.getMinutes()
+                else minutes = currDate.getMinutes();
+
+                var timeEntered = $('<span class="time">' + (currDate.getHours() % 12) + ':' + minutes + AMPM + '</span>');
+
+                var dateEntered = $('<span class="date">' + currDate.getMonth() + '/' + currDate.getDate() + '/' + currDate.getFullYear() + '</span>');
+
+                //new task HTML w/ icons appended
+                var newTask = $('<li class="task">' + task + '</li>').append(timeEntered, dateEntered, deleteIcon, doneIcon);
+
+                $('#list ul').append(newTask);  // add newTask
+            }
 
             return false;
-        })
+    })
 })
