c3faa7e3a07efb0be697d5ecca8e451bfe328be6
jameskane05 <jameskane05@gmail.com>
2015-05-21 16:53:58 -0500

    Final update - committing to fetch new getitdone-jquery README

--- getitdone-jquery/js/getitdone.js
+++ getitdone-jquery/js/getitdone.js
@@ -7,30 +7,30 @@ $(document).ready(function() {	// waits for whole page to load
 
                 // func to run when tasks are completed
                 var taskDone = function () {
-                    $('#done ul').append(this.parentNode);
+                    $('#done ul').append(this.parentNode.parentNode);
                         /* this will refer to the obj on which .click is called,
                            a glyphicon span element whose parent node li contains
                            the entire newTask obj */
                     $('.glyphicon-ok', '#done').remove();  //remove any elements with class glyphicon-ok
-                    $(newTask).append(repeatIcon.click(taskRepeat));
+                    $(infoDiv).append(repeatIcon.click(taskRepeat));
                 };
 
                 // func that puts the task back on the #list
                 var taskRepeat = function () {
-                    $('#list ul').append(this.parentNode);
+                    $('#list ul').append(this.parentNode.parentNode);
                     $('.glyphicon-repeat', '#list').remove();
-                    $(newTask).append(doneIcon.click(taskDone));
+                    $(infoDiv).append(doneIcon.click(taskDone));
                 };
 
                 // func that removes tasks
                 var taskDelete = function () {
-                    $(this.parentNode).remove();
+                    $(this.parentNode.parentNode).remove();
                 };
 
                 //glyphicon HTML and .click callback funcs
-                var deleteIcon = $('<span class="glyphicon glyphicon-trash"></span>').click(taskDelete);
-                var doneIcon = $('<span class="glyphicon glyphicon-ok"></span>').click(taskDone);
-                var repeatIcon = $('<span class="glyphicon glyphicon-repeat"></span>').click(taskRepeat);
+                var deleteIcon = $('<div class="col-sm-3 glyphicon glyphicon-trash"></div>').click(taskDelete);
+                var doneIcon = $('<div class="col-sm-3 glyphicon glyphicon-ok"></div>').click(taskDone);
+                var repeatIcon = $('<div class="col-sm-3 glyphicon glyphicon-repeat"></div>').click(taskRepeat);
 
                 var currDate = new Date($.now());  //get current date
 
@@ -43,18 +43,27 @@ $(document).ready(function() {	// waits for whole page to load
                 if (currDate.getMinutes() < 10) var minutes = "0" + currDate.getMinutes();
                 else minutes = currDate.getMinutes();
 
+                //mod getHours() by 12 - if noon, display as "12:00", not "0:00"
+                var hours = currDate.getHours();
+                if (hours == 12) hours = "12";
+                else hours = currDate.getHours() % 12;
+
                 //format current time and date, wrap as jQuery obj
-                var time = $('<span class="time">' + (currDate.getHours() % 12) + ':' + minutes + amPm + '</span>');
-                var date = $('<span class="date">' + currDate.getMonth() + '/'
-                    + currDate.getDate() + '/' + currDate.getFullYear() + '</span>');
+                var time = $('<div class="col-sm-3 time">' + hours + ':' + minutes + amPm + '</div>');
+                var date = $('<div class="col-sm-3 date">' + currDate.getMonth() + '/'
+                    + currDate.getDate() + '/' + currDate.getFullYear() + '</div>');
 
-                //new task HTML w/ icons appended
-                var newTask = $('<li class="task">' + task + '</li>')
-                    .append(time, date, deleteIcon, doneIcon);
+                //build up new task HTML w/ icons appended
+                var newTask = $('<li class="row"></li>');
+                var taskDiv = $('<div class="col-lg-6 taskInfo">' + task + '</div>');
+                var infoDiv = $('<div class="col-lg-6 infoDiv"></div>');
+                infoDiv.append(time, date, deleteIcon, doneIcon);
+                newTask.append(taskDiv, infoDiv);
 
-                $('#list ul').append(newTask);  // add newTask
+                $('#list ul').append(newTask);  //add newTask
             }
 
+            $('#taskEntry').val('');
             //don't return to server
             return false;
     })
